237ef3ee9f6d1ebc6915b80f3d6aba5a72c3eef2
opentelemetrybot <107717825+opentelemetrybot@users.noreply.github.com>
2026-04-29 02:22:45 +0000

    Add JMX metrics gatherer version 1.56.0-alpha

--- receiver/jmxreceiver/supported_jars.go
+++ receiver/jmxreceiver/supported_jars.go
@@ -31,6 +31,10 @@ func oldFormatProperties(c *Config, j supportedJar) error {
 // If you change this variable name, please open an issue in opentelemetry-java-contrib
 // so that repository's release automation can be updated
 var jmxMetricsGathererVersions = map[string]supportedJar{
+	"889b32f2875e6d1816f1fd8baa0dfb6c80002ad7fd382e4679e1c92d97ffa999": {
+		version: "1.56.0-alpha",
+		jar:     "JMX metrics gatherer",
+	},
 	"9f4f7ab6fe7040dbeb91fae75dc17199c408a6339eb498d2f29088b78c621c2c": {
 		version: "1.31.0-alpha",
 		jar:     "JMX metrics gatherer",
